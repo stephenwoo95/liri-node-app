558d82c8c4699797398dfb4b27f719efd2cf12be
Stephen Woo <stephenwoo95@gmail.com>
2017-08-15 14:44:20 -0700

    finished basic functionality no Bonus

--- keys.js
+++ keys.js
@@ -1,5 +1,3 @@
-console.log('this is loaded');
-
 exports.twitterKeys = {
   consumer_key: 'ICIJR4xP23c3tDwB7DmeHlfn5',
   consumer_secret:'TTsfejKWTQZklxUGiagN18qVklLTdgW7TEocrFWhrCMncHJPMS',
@@ -8,8 +6,8 @@ exports.twitterKeys = {
 }
 
 exports.spotifyKeys = {
-	client_id: '05f7598d0d744dc3be2886dadf3413f0',
-	client_secret: 'd679375d0bdd4b098550053d92466486'
+	id: '05f7598d0d744dc3be2886dadf3413f0',
+	secret: 'd679375d0bdd4b098550053d92466486'
 }
 
 exports.omdbKey = {
--- liri.js
+++ liri.js
@@ -1,11 +1,106 @@
 //Grab keys for APIs
 var twitterKeys = require("./keys.js").twitterKeys;
-console.log(twitterKeys);
+// console.log(twitterKeys);
 var spotifyKeys = require("./keys.js").spotifyKeys;
-console.log(spotifyKeys);
+// console.log(spotifyKeys);
 var omdbKey = require("./keys.js").omdbKey;
-console.log(omdbKey);
+// console.log(omdbKey);
+
+var twitter = require('twitter');
+var spotify = require('node-spotify-api');
+var request = require('request');
 var fs = require("fs");
+
+var moment = require('moment');
+
+var client = new twitter(twitterKeys);
+var spotify = new spotify(spotifyKeys);
+
 
 //Grab command line command
 var command = process.argv.slice(2);
+var title = command.slice(1).join('+');
+
+function query(cmd,title){
+	switch(cmd){
+		case 'my-tweets':
+			client.get('statuses/home_timeline',{count:20}, function(error, tweets, response){
+					if(error)
+						console.log('error: '+error);
+					else{
+						for(i=0;i<tweets.length;i++){
+							console.log('=================\n');
+							console.log(tweets[i]['created_at']);
+							console.log(tweets[i].text+'\n');
+						}
+					}
+				});
+			break;
+		case 'spotify-this-song':
+			if(title == '')
+				title = 'The+Sign%20artist:Ace+of+Base';
+			spotify.search({
+				type:'track',
+				query: title
+			}, function(err,data){
+				if(err)
+					return console.log('Error occured: '+err);
+				else{
+					console.log('Artist: ' + data.tracks.items[0].artists[0].name);
+					console.log('Track Name: '+data.tracks.items[0].name);
+					if(data.tracks.items[0].preview_url==null)
+						console.log('No Preview Avaialable');
+					else
+						console.log('Preview URL: '+data.tracks.items[0].preview_url);
+					console.log('Album Name: '+data.tracks.items[0].album.name);
+				}
+			});
+			break;
+		case 'movie-this':
+			if(title == '')
+				title = 'Mr.+Nobody';
+			var queryURL = 'https://www.omdbapi.com/?apikey='+omdbKey.key+'&plot=short&t='+title;
+			request(queryURL,function(error,response,body){
+				if(error)
+					return console.log('error:',error);
+				else if(response.statusCode == 200){
+					var parsed = JSON.parse(body);
+					console.log('Title: ' + parsed['Title']);
+					console.log('Release Year: '+parsed.Year);
+					console.log('IMDB Rating: '+parsed.Ratings[0].Value);
+					console.log('Rotten Tomatoes Rating: '+parsed.Ratings[1].Value);
+					console.log('Country of Origin: '+parsed.Country);
+					console.log('Language: '+parsed.Language);
+					console.log('Plot: '+parsed.Plot);
+					console.log('Actors: '+parsed.Actors);
+				}
+			});
+			break;
+		case 'do-what-it-says':
+			fs.readFile('random.txt', 'utf8',function(error, data){
+				if(error)
+					console.log('error: ' + error);
+				else{
+					var dataArr = data.split(',');
+					query(dataArr[0],dataArr[1]);
+				}
+			});
+			break;
+		default:
+			break;
+	};
+}
+
+function twitterCmd(){
+
+}
+
+function spotifyCmd(){
+
+}
+
+function omdbCmd(){
+
+}
+
+query(command[0],title);
